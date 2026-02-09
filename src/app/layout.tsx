98f01746eea1bf13f227ed22f30cd80f7c191fbf
svomargosantoso <svomargosantoso@gmail.com>
2026-02-09 14:06:54 +0700

    Add Meta Pixel (Facebook Pixel) tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,6 +57,33 @@ export default function RootLayout({
       <head>
         {/* Adcuan Tracking Script */}
         <script src="https://plus.adcuan.com/code.js?nomor=4" async></script>
+        
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '864070406332606');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=864070406332606&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
